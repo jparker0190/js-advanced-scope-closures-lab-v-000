a05e164f3105d07a964076e0ec616de5ebbffd51
Joseph Parker <jparker010190@gmail.com>
2018-07-13 15:41:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function produceDrivingRange(blockRange){
   return function(newblockRange){
-     if (blockRange > 10){
+     if (blockRange < 10){
        let result = 10 - blockRange
        return `${result} blocks out of range`
      }
-     else if (blockRange < 10){
+     else if (blockRange > 10){
        let result = blockRange - 10
        return `within range by ${result}`
      }
